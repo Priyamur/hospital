6cf703dd3f7ba6d8a4a233b167b253a3c28beec5
Priyadharshini Murugan <priyadharshini.mu@itp.objectfrontier.com>
2023-09-07 12:38:37 +0530

    first release

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+public abstract class Hospital
+{
+    public abstract void DoctorLists();
+    public abstract void BedLists();
+    {
+        Console.WriteLine("beds count at hospital ")
+    }
+}
